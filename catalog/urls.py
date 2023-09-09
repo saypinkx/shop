b1a4b79bc58979120818658d2692f314f16a5d8f
sixzerx <paprut538@gmail.com>
2023-09-09 23:27:33 +0300

    add catalog template

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import CatalogView
 urlpatterns = [
-    path('<category>', CatalogView.as_view()),
+    path('<category>', CatalogView.as_view(), name='catalog'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from .models import CatalogModel
+
 
 # Create your views here.
 class CatalogView(View):
     def get(self, request, category):
-        return HttpResponse(f'hello {category}')
+        data = {
+            'product': CatalogModel.objects.all(),
+
+        }
+        data['one'] = data['product'][0].imagemodel_set.all()[0].image
+        return render(request, 'catalog/catalog.html', context=data)
